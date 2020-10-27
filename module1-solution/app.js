9d4cd44ab84860631c869196c2ef5aecac433fc7
Aleksander Altmäe <aleksander.altmae@tptlive.ee>
2020-10-27 21:25:44 +0200

    add colors to text and borders

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,21 +9,28 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
     $scope.dishes = "";
     $scope.message = "";
+    $scope.colorText = false;
     
     $scope.CheckIfTooMuch = function () {
         var dishCount = $scope.dishes.split(",").filter($scope.isNotEmpty).length; //will not consider and empty item
         if (dishCount == 0) {
             $scope.message = "Please enter data first";
+            $scope.colorText = "red";
+            $scope.borderColorText = "red";
         }
         else if (dishCount >= 0 && dishCount <= 3) {
           $scope.message = "Enjoy!";
+          $scope.colorText = "green";
+          $scope.borderColorText = "green";
         }
         else {
           $scope.message = "Too much!";
+          $scope.colorText = "green";
+          $scope.borderColorText = "green";
         }
     };
 
-    $scope.isNotEmpty=function(value){
+    $scope.isNotEmpty = function (value) {
         return (value == "") ? false : true;
     };
 
